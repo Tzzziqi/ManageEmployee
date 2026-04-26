277d1586d1a0ab4040476ea2006b2da9ece66bd5
Freya379 <125752050+Freya379@users.noreply.github.com>
2026-04-26 11:07:30 -0700

    document model, HR aslo need it

---
+++ backend/models/document.js
@@ -0,0 +1,29 @@
+// Documnet.js to track who's docs belong to who and the status, location and type
+
+const mongoose = reqiure('mongoose');
+
+const DocumentSchema = new mongoose.Schema({
+    employeeId: {
+        type: mongoose.Shema.Type.ObjectId,
+        ref: 'Employee',
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: ['Profile_Pic', 'Drivers_License', 'Work_Auth', 'Opt_Receipt', 'OPT_EDA','I983', '-20'],
+        required: true
+    },
+    fileUrl: { type: String, required: true },
+    fileKey: { type: String, required: true },
+
+    status: {
+        type: String,
+        enum: ['Pending', 'Approved', 'Rejected'],
+        default: 'Pending'
+    },
+    feedback: String,
+
+    uploadedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Document', DocumentSchema);
