886736d57ef4da131125243be5483a17713c93f5
Tzzziqi <btysxq@gmail.com>
2026-04-30 00:43:06 -0700

    update ProtectedRoute

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import VerifyInvitePage from "./pages/VerifyInvitePage.tsx";
 import RegistrationPage from "./pages/RegistrationPage.tsx";
 import HRApplicationsPage from "./pages/HRApplicationsPage.tsx";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
     return (
@@ -9,7 +10,14 @@ function App() {
             <Routes>
                 <Route path="/WelcomeToChuwa/:inviteToken" element={ <VerifyInvitePage/> }/>
                 <Route path="/register/:inviteToken" element={ <RegistrationPage/> }/>
-                <Route path="/hr/onboarding" element={<HRApplicationsPage />} />
+                <Route
+                    path="/hr/onboarding"
+                    element={
+                        <ProtectedRoute allowedRole="hr">
+                        <HRApplicationsPage />
+                        </ProtectedRoute>
+                    }
+                />
                 <Route path="/" element={<Navigate to="/hr/onboarding" replace />} />
             </Routes>
         </div>
--- frontend/src/store/slices/authSlice.ts
+++ frontend/src/store/slices/authSlice.ts
@@ -3,57 +3,66 @@ import axios from "axios";
 import authService, { type SignUpProps } from "../../services/authService";
 
 export const signup = createAsyncThunk(
-    'auth/signup',
-    async (props: SignUpProps, thunkAPI) => {
-        try {
-            return await authService.signup(props);
-        } catch (error) {
-            let message;
-            if (axios.isAxiosError(error)) {
-                message = error.response?.data?.message || error.message || 'Unknown error';
-            }
-            return thunkAPI.rejectWithValue(message);
-        }
+  "auth/signup",
+  async (props: SignUpProps, thunkAPI) => {
+    try {
+      return await authService.signup(props);
+    } catch (error) {
+      let message = "Unknown error";
+
+      if (axios.isAxiosError(error)) {
+        message = error.response?.data?.message || error.message || "Unknown error";
+      }
+
+      return thunkAPI.rejectWithValue(message);
     }
-)
+  }
+);
 
 const initialState = {
-    username: null,
-    token: localStorage.getItem('token') || null,
-    isAuthenticated: !!localStorage.getItem('token'),
-    status: 'idle',
-    error: '',
-}
+  username: null as string | null,
+  role: localStorage.getItem("role") || null,
+  token: localStorage.getItem("token") || null,
+  isAuthenticated: !!localStorage.getItem("token"),
+  status: "idle",
+  error: "",
+};
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        logout: (state) => {
-            state.username = null;
-            state.token = null;
-            state.isAuthenticated = false;
-            localStorage.removeItem('token');
-        },
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.username = null;
+      state.role = null;
+      state.token = null;
+      state.isAuthenticated = false;
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("role");
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(signup.pending, (state) => {
-                state.status = 'loading';
-            })
-            .addCase(signup.fulfilled, (state, action) => {
-                state.status = 'succeeded';
-                state.username = action.payload.username;
-                state.token = action.payload.token;
-                state.isAuthenticated = true;
-                localStorage.setItem('token', action.payload.token);
-            })
-            .addCase(signup.rejected, (state, action) => {
-                state.status = 'failed';
-                state.error = action.payload as string;
-            })
-    }
-})
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signup.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(signup.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.username = action.payload.username;
+        state.role = action.payload.role;
+        state.token = action.payload.token;
+        state.isAuthenticated = true;
+
+        localStorage.setItem("token", action.payload.token);
+        localStorage.setItem("role", action.payload.role);
+      })
+      .addCase(signup.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload as string;
+      });
+  },
+});
 
 export const { logout } = authSlice.actions;
 export default authSlice.reducer;
